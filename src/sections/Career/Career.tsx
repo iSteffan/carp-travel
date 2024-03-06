5fd27838010b4a85c27b31193ed694cd3d2704ec
Stepan Skichko <st.skichko@gmail.com>
2024-03-06 17:55:08 +0200

    career tablet markup done (except form)

--- src/sections/Career/Career.tsx
+++ src/sections/Career/Career.tsx
@@ -3,46 +3,66 @@ import css from './Career.module.css';
 const Career = () => {
   return (
     <section className={css.section}>
-      <h2 className="title mb-[24px]">
-        Choose <span className="font-medium">US</span>
-      </h2>
-      <p className="mb-[36px] ml-auto w-[179px] text-[14px] text-gray text-gray font-extralight leading-[1.43]">
-        Your chance to join our passionate team in Carpathian tourism. Seeking talented
-        professionals to share our common mission.
-      </p>
+      <div className="md:grid md:grid-cols-[211px,20px,127px,94px,21px,221px] md:grid-rows-[8px,73px,7px,5px,36px,56px,60px,32px,316px]">
+        <h2 className="title mb-[24px] md:col-start-1 md:col-span-4 md:row-start-1 md:row-span-2 md:mb-0">
+          Choose <span className="font-medium">US</span>
+        </h2>
+        <p
+          className="mb-[36px] ml-auto w-[179px] text-[14px] text-gray font-extralight leading-[1.43] 
+        md:col-start-6 md:col-span-1 md:row-start-2 md:row-span-2 md:mb-0 md:w-full md:text-[13px] md:leading-[1.54]"
+        >
+          Your chance to join our passionate team in Carpathian tourism. Seeking talented
+          professionals to share our common mission.
+        </p>
 
-      <p className="ml-auto mb-[36px] w-[179px] text-[30px] font-extralight">WHY US?</p>
+        <p
+          className="ml-auto mb-[36px] w-[179px] text-[30px] font-extralight
+        md:col-start-1 md:col-span-1 md:row-start-5 md:row-span-1 md:w-full md:m-0 md:text-right"
+        >
+          WHY US?
+        </p>
 
-      <ul className="list-none flex flex-col gap-[16px] mr-auto w-[181px] text-end">
-        <li>
-          <p className="mb-[8px] text-[14px] leading-[1.43] font-normal">
-            Professional development
-          </p>
-          <p className="text-[12px] font-extralight leading-[1.67]">
-            We offer growth opportunities and a creative environment to nurture your talents.
-          </p>
-        </li>
-        <li>
-          <p className="mb-[8px] text-[14px] leading-[1.43] font-normal">Teamwork</p>
-          <p className="text-[12px] font-extralight leading-[1.67]">
-            Join our close-knit family, where support and inspiration abound.
-          </p>
-        </li>
-        <li>
-          <p className="mb-[8px] text-[14px] leading-[1.43] font-normal">
-            Stimulating work environment
-          </p>
-          <p className="text-[12px] font-extralight leading-[1.67]">
-            Flexibility and remote options for a comfortable experience.
-          </p>
-        </li>
-        <li>
-          <p className="mb-[8px] text-[14px] leading-[1.43] font-normal">Exciting challenges</p>
-          <p className="text-[12px] font-extralight leading-[1.67]">
-            Unleash your potential through unforgettable projects showcasing Carpathian beauty.
-          </p>
-        </li>
-      </ul>
+        <ul
+          className="list-none flex flex-col gap-[16px] mr-auto w-[181px] text-end 
+        md:col-start-1 md:col-span-1 md:row-start-7 md:row-span-1 md:w-full md:gap-[24px]"
+        >
+          <li>
+            <p className="mb-[8px] text-[14px] leading-[1.43] font-normal md:text-[16px] md:leading-[1.25]">
+              Professional development
+            </p>
+            <p className="text-gray text-[12px] font-extralight leading-[1.67]">
+              We offer growth opportunities and a creative environment to nurture your talents.
+            </p>
+          </li>
+          <li>
+            <p className="mb-[8px] text-[14px] leading-[1.43] font-normal">Teamwork</p>
+            <p className="text-gray text-[12px] font-extralight leading-[1.67]">
+              Join our close-knit family, where support and inspiration abound.
+            </p>
+          </li>
+          <li>
+            <p className="mb-[8px] text-[14px] leading-[1.43] font-normal md:mb-[28px]">
+              Stimulating work environment
+            </p>
+            <p className="text-gray text-[12px] font-extralight leading-[1.67]">
+              Flexibility and remote options for a comfortable experience.
+            </p>
+          </li>
+          <li>
+            <p className="mb-[8px] text-[14px] leading-[1.43] font-normal">Exciting challenges</p>
+            <p className="text-gray text-[12px] font-extralight leading-[1.67]">
+              Unleash your potential through unforgettable projects showcasing Carpathian beauty.
+            </p>
+          </li>
+        </ul>
+        <p
+          className="max-md:hidden text-[13px] text-gray font-extralight leading-[1.54] 
+        md:col-start-3 md:col-span-2 md:row-start-7 md:row-span-1"
+        >
+          Don't miss your opportunity! <br /> Fill out the form right now <br />
+          and join our team!
+        </p>
+      </div>
     </section>
   );
 };

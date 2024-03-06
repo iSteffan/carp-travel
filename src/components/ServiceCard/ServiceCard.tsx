e67f5e767869bca96d29a55a2b2c18b366e351f1
Stepan Skichko <st.skichko@gmail.com>
2024-03-06 15:24:07 +0200

    add dynamic render to service slider / start trials with nav btn

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -1,7 +1,15 @@
-import { Services1 } from '@/assets/images';
 import Image from 'next/image';
 
-const ServiceCard = ({ data }) => {
+export interface ServiceCardProps {
+  data: {
+    img: string;
+    desc: string;
+    alt: string;
+    call: string;
+  };
+}
+
+const ServiceCard = ({ data }: ServiceCardProps) => {
   return (
     <div className="w-full pt-[54px] px-[20px] pb-[56px] grid grid-cols-1 grid-rows-[56px,24px,52px,16px,213px,12px,24px,24px,166px,34px,120px]">
       <h2
@@ -16,19 +24,18 @@ const ServiceCard = ({ data }) => {
       </p>
 
       <Image
-        src={Services1}
+        src={data.img}
         alt="image"
         width={280}
+        height={213}
         className="row-start-5 row-end-6 object-cover h-full"
       />
 
       <p className="row-start-7 row-end-8 text-end text-[12px] text-gray font-extralight leading-[2] tracking-[2.4px]">
-        Feel the adrenaline rush
+        {data.call}
       </p>
       <p className="row-start-11 row-end-12 text-[14px] text-gray font-extralight leading-[1.43]">
-        Join exciting rafting expeditions on the waterways of the Carpathians. Go through
-        challenging waterways and overcome gusty waves, feel the adrenaline, and enjoy the
-        incredible views of the surrounding mountains.
+        {data.desc}
       </p>
     </div>
   );
--- src/components/ServicesSlider/ServicesSlider.tsx
+++ src/components/ServicesSlider/ServicesSlider.tsx
@@ -3,13 +3,12 @@
 import 'swiper/css/pagination';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
-import { GallerySlider1, GallerySlider2, GallerySlider3 } from '@/assets/images';
 
-import Image from 'next/image';
 import './styles.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import ServiceCard from '../ServiceCard/ServiceCard';
 import { EffectFade, Pagination } from 'swiper/modules';
+import { servicesData } from '@/data/servicesData';
 
 export const ServicesSlider = () => {
   return (
@@ -22,56 +21,15 @@ export const ServicesSlider = () => {
         clickable: true,
       }}
       modules={[EffectFade, Pagination]}
-      className="mySwiper"
-      //   autoHeight={true}
-      //   slidesPerView={1}
-      //   cssMode={true}
-      //   spaceBetween={30}
-      //   effect={'fade'}
-      //   centeredSlides={true}
-      //   pagination={{
-      //     clickable: true,
-      //   }}
-      //   modules={[EffectFade, Pagination]}
-      //   className="mySwiper"
+      className="mySwiper service"
     >
-      {/* {galleryData.map((item, index) => {
+      {servicesData.map((item, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="img-overlay">
-              <Image
-                fill={true}
-                src={item.img}
-                alt="gallery slider image"
-                width={0}
-                height={0}
-                className="w-auto h-auto"
-              />
-            </div>
+            <ServiceCard data={item} />
           </SwiperSlide>
         );
-      })} */}
-      {/* <SwiperSlide>
-        <ServiceCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ServiceCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ServiceCard />
-      </SwiperSlide> */}
-      <SwiperSlide>
-        <Image src={GallerySlider1} alt="gallery slider image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={GallerySlider2} alt="gallery slider image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={GallerySlider3} alt="gallery slider image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={GallerySlider2} alt="gallery slider image" />
-      </SwiperSlide>
+      })}
     </Swiper>
   );
 };
